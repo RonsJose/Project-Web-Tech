246b2544072f3000f5f951f3d709cb31dddc43a9
RonsJose <g00464215@atu.ie>
2024-11-21 12:46:42 +0000

    Added Popup
    Added the popup for all the pages

---
+++ Rons_script.js
@@ -0,0 +1,23 @@
+function openPopUpLegends(){
+    document.getElementById("PopUpLegends").style.display = "flex";
+}
+
+function closePopUpLegends(){
+    document.getElementById("PopUpLegends").style.display = "none";
+}
+
+function openPopUpDungeons(){
+    document.getElementById("PopUpDungeons").style.display = "flex";
+}
+
+function closePopUpDungeons(){
+    document.getElementById("PopUpDungeons").style.display = "none";
+}
+
+function openPopUpJavaBedrock(){
+    document.getElementById("PopUpJavaBedrock").style.display = "flex";
+}
+
+function closePopUpJavaBedrock(){
+    document.getElementById("PopUpJavaBedrock").style.display = "none";
+}
